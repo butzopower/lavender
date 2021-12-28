5aea100505cdd4159fcbf859268d219992f2100c
Brian Butz <butzopower@gmail.com>
2021-12-28 21:57:31 +0000

    initialize

---
+++ tests/explore/explore.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it } from 'mocha';
+import * as assert from "assert";
+
+describe('some test', () => {
+   it('does a thing', () => {
+      assert.equal("abc", "def")
+   });
+});
